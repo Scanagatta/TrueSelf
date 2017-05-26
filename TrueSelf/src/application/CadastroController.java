cef0c60ec13b2656bedf8f4d758dd1f3a747deaf
Andre Vicensi <andrevicensi@yahoo.com.br>
2017-05-25 21:21:16 -0300

    Agora so faz o usuario se a senha esta certa

--- TrueSelf/src/application/CadastroController.java
+++ TrueSelf/src/application/CadastroController.java
@@ -18,114 +18,114 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class CadastroController {
-	
-    @FXML
-    private AnchorPane apPrincipal;
-	
+
 	@FXML
-    private TextField tfNome;
+	private AnchorPane apPrincipal;
 
-    @FXML
-    private TextField tfTelefone;
+	@FXML
+	private TextField tfNome;
 
-    @FXML
-    private DatePicker dtNascimento;
+	@FXML
+	private TextField tfTelefone;
 
-    @FXML
-    private TextField tfLogin;
+	@FXML
+	private DatePicker dtNascimento;
 
-    @FXML
-    private ComboBox<String> cbEstadoCivil;
+	@FXML
+	private TextField tfLogin;
 
-    @FXML
-    private PasswordField pfSenha;
+	@FXML
+	private ComboBox<String> cbEstadoCivil;
 
-    @FXML
-    private PasswordField cpsSenha;
+	@FXML
+	private PasswordField pfSenha;
 
-    @FXML
-    private Button btnSalvar;
+	@FXML
+	private PasswordField cpsSenha;
 
-    @FXML
-    private Button btnVoltar;
-    
-    private Usuario usuario;
-    
-    public void initialize(){
-    	cbEstadoCivil.setItems(FXCollections.observableArrayList("solteiro","namorando","casado","divorciado"));
-    }
-    
-    public void novo(){
-    	usuario = new Usuario();
-    }
-    
-    @FXML
-    void onVoltar(ActionEvent event) {
-    	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("telaPrimeira.fxml"));
-    	try{
-    		AnchorPane principal1View = (AnchorPane) loader.load();
-    		TelaPrincipal.root.setCenter(principal1View);
-    		
-    	} catch (IOException e1) {
-    		e1.printStackTrace();
-    	}
-    }
-    
-    @FXML
-    void onsalvar(ActionEvent event) {
-    	novo();
-    	usuario.setNome(tfNome.getText());
-    	usuario.setTelefone(tfTelefone.getText());
-    	usuario.setDataNascimento(dtNascimento.getValue());
-    	usuario.setLogin(tfLogin.getText());
-    	usuario.setEstadoCivil(cbEstadoCivil.getValue());
-    	conferirSenha();
-    	SimuladorDB.insert(usuario);
-    	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("telaLogin.fxml"));
-    	try{
-    		AnchorPane loginView = (AnchorPane) loader.load();
-    		TelaPrincipal.root.setCenter(loginView);
-    		
-    	} catch (IOException e1) {
-    		e1.printStackTrace();
-    	}
-    }
-    
-    public boolean conferirSenha(){
-    	String senha = pfSenha.getText();
-    	String confirmaSenha = cpsSenha.getText();
-    	// ver se as senhas estao vazias
-    	if(senha.isEmpty() || confirmaSenha.isEmpty()){
-    		Alert alert = new Alert(AlertType.ERROR);
-    		alert.setTitle("Erro");
+	@FXML
+	private Button btnSalvar;
+
+	@FXML
+	private Button btnVoltar;
+
+	private Usuario usuario;
+
+	public void initialize() {
+		cbEstadoCivil.setItems(FXCollections.observableArrayList("solteiro", "namorando", "casado", "divorciado"));
+	}
+
+	public void novo() {
+		usuario = new Usuario();
+	}
+
+	@FXML
+	void onVoltar(ActionEvent event) {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("telaPrimeira.fxml"));
+		try {
+			AnchorPane principal1View = (AnchorPane) loader.load();
+			TelaPrincipal.root.setCenter(principal1View);
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	@FXML
+	void onsalvar(ActionEvent event) {
+		novo();
+		usuario.setNome(tfNome.getText());
+		usuario.setTelefone(tfTelefone.getText());
+		usuario.setDataNascimento(dtNascimento.getValue());
+		usuario.setLogin(tfLogin.getText());
+		usuario.setEstadoCivil(cbEstadoCivil.getValue());
+		if (conferirSenha()) {
+			SimuladorDB.insert(usuario);
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("telaLogin.fxml"));
+			try {
+				AnchorPane loginView = (AnchorPane) loader.load();
+				TelaPrincipal.root.setCenter(loginView);
+
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public boolean conferirSenha() {
+		String senha = pfSenha.getText();
+		String confirmaSenha = cpsSenha.getText();
+		// ver se as senhas estao vazias
+		if (senha.isEmpty() || confirmaSenha.isEmpty()) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Erro");
 			alert.setHeaderText("Ação inválida");
 			alert.setContentText("Preencha os campos");
 			alert.showAndWait();
 			limparSenha();
 			return false;
-    		
-    	}
-    	// ver se as senhas sao iguais
-    	if(senha.equals(confirmaSenha)){
-    		usuario.setSenha(senha);
-    		return true;
-    	} else {
-    		Alert alert = new Alert(AlertType.ERROR);
-    		alert.setTitle("Erro");
+
+		}
+		// ver se as senhas sao iguais
+		if (senha.equals(confirmaSenha)) {
+			usuario.setSenha(senha);
+			return true;
+		} else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Erro");
 			alert.setHeaderText("As senhas não são iguais");
 			alert.setContentText("Digite a senha corretamente");
 			alert.showAndWait();
 			limparSenha();
 			return false;
-    	}
-    }
-    
-    public void limparSenha(){
-    	pfSenha.setText("");
-    	cpsSenha.setText("");
-    }
-    
+		}
+	}
+
+	public void limparSenha() {
+		pfSenha.setText("");
+		cpsSenha.setText("");
+	}
 
 }
